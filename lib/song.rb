5c8712225bc98bad6fd449b663f358960d035030
Timothy Lalli <trlalli92@gmail.com>
2020-07-20 02:43:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,8 @@ class Song
   
   def self.new_from_filename(name)
     split = name.split(' - ')
-    
+    artist_name = split[0]
+    song = split[1]
   end
   
   def self.create_from_filename(name)
